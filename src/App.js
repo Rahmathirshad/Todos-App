c417a7601b0b371fafad0c84f0cda1355a9e3928
Rahmath_Irshad <rahmathirshad@gmail.com>
2023-01-15 07:25:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './MyComponents/Header'; 
+import {Todos} from './MyComponents/Todos'; 
+import { AddTodo } from './MyComponents/AddTodo';
+import {Footer} from './MyComponents/Footer';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  let todosLocalStorage;
+  
+  if(localStorage.getItem("todosLocalStorage")===null){ //if localstorage is null then run below code
+    todosLocalStorage = [];
+  }
+  else{
+    todosLocalStorage =JSON.parse(localStorage.getItem("todosLocalStorage"));
+  }
+
+  const [todosState, setTodosState] = useState(todosLocalStorage); // initial todos
+
+  // Function to Add Todos
+  const addTodo = (title, desc) => {
+    let sno = todosState.length + 1; // generating serial no's
+    
+    const newTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodosState([...todosState, newTodo]);
+  }
+
+  // Function to delete Todos 
+  const onDelete = (todoItem) => {
+    setTodosState(todosState.filter((num)=>{
+      return num !== todoItem;
+    }));
+    localStorage.setItem("todosLocalStorage", JSON.stringify(todosState)); // deleting local storage
+  }
+  
+  //using state hook (todos1 is current state & setTodos1 is alter state)
+  useEffect(() => {
+    localStorage.setItem("todosLocalStorage", JSON.stringify(todosState)); // setting local storage
+  
+  }, [todosState]) // when my todos1 gets change, useEffect gets call & runs above method & saves new todos in local storage
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='vh-100'>
+    <Header/>
+    <AddTodo addTodo={addTodo} />
+    <Todos todosState = {todosState} onDelete = {onDelete} />
+    <Footer/>
     </div>
   );
 }
